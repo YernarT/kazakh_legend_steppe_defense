e36d2a40c2f6cdfa64bbfccbb635601d6ef932e3
YernarT <toktaryernar@gmail.com>
2025-08-28 11:43:19 +0500

    refactor: :recycle: 重构导入路径以优化代码结构

--- client/src/renderer/core/index.ts
+++ client/src/renderer/core/index.ts
@@ -1,11 +1,11 @@
-import { getBaiterek } from "@/core/baiterek";
-import { getKhanshatyr } from "@/core/khanshatyr";
-import { getDombra } from "@/core/dombra";
-import { getKobyz } from "@/core/kobyz";
-import { getFeltHouse } from "@/core/felt-house";
-import { getRug } from "@/core/rug";
-import { getSamovar } from "@/core/samovar";
-import { getEagle } from "@/core/eagle";
+import { getBaiterek } from "@/core/plants/baiterek";
+import { getKhanshatyr } from "@/core/plants/khanshatyr";
+import { getDombra } from "@/core/plants/dombra";
+import { getKobyz } from "@/core/plants/kobyz";
+import { getFeltHouse } from "@/core/plants/felt-house";
+import { getRug } from "@/core/plants/rug";
+import { getSamovar } from "@/core/plants/samovar";
+import { getEagle } from "@/core/plants/eagle";
 
 export {
   getBaiterek,
