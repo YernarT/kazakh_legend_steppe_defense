8afb48fc8cc17263ca0efaea7644e1eda38164bf
YernarT <toktaryernar@gmail.com>
2025-09-01 23:01:54 +0500

    update: :hammer: 白天在游戏中随机掉落阳光

--- client/src/renderer/views/game.vue
+++ client/src/renderer/views/game.vue
@@ -15,15 +15,49 @@
 
 <script setup lang="ts">
 // Vue
-import { onMounted, ref } from "vue";
-// Types
-import type { I_Plant } from "@/typing/plant";
+import { watchEffect } from "vue";
+// Store
+import { useSceneStore } from "@/store/useSceneStore";
+// Core
+import { dropSun } from "@/core/canvas/dropSun";
 // Hooks
 import { useCanvas } from "@/core/canvas/useCanvas";
 // Components
 import ToolsBox from "@/components/ToolsBox/index.vue";
+// Constants
+import { GAME_STATUS } from "@/constants/game-status";
+// Utils
+import { random } from "@/utils/random";
 
+const sceneStore = useSceneStore();
 const { Ref_Canvas, ctx } = useCanvas();
+
+function scheduleDropSun() {
+  setTimeout(() => {
+    const isCanvasReady =
+      Ref_Canvas.value &&
+      ctx.value &&
+      Ref_Canvas.value.width &&
+      Ref_Canvas.value.height;
+
+    if (!isCanvasReady) scheduleDropSun();
+
+    const randomX = random(80, Ref_Canvas.value!.width - 80);
+    const randomY = random(0, 20);
+    dropSun({ ctx: ctx.value!, x: randomX, y: randomY });
+    scheduleDropSun();
+  }, random(2, 5) * 1000);
+}
+
+watchEffect(() => {
+  const isDay = sceneStore.isDay;
+  const isPlaying = sceneStore.gameStatus === GAME_STATUS.playing;
+
+  // 白天在游戏中随机掉落阳光
+  if (isDay && isPlaying) {
+    scheduleDropSun();
+  }
+});
 </script>
 
 <style scoped lang="scss">
